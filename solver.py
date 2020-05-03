469f84ab2ba5a31b6f0e300bcf066cbe351b958b
Connie Huang <conhuang@berkeley.edu>
2020-05-02 23:48:40 -0700

    started dom set solution

--- solver.py
+++ solver.py
@@ -1,8 +1,29 @@
 import networkx as nx
 from parse import read_input_file, write_output_file
-from utils import is_valid_network, average_pairwise_distance
+from utils import is_valid_network, average_pairwise_distance_fast
 import sys
 
+class DisjointSet:
+    def __init__(self, vertices):
+        self.vertices = [[v, 0] for v in range(vertices)]
+
+    def contains(self, vertex):
+        return vertex in [x[0] for x in self.vertices]
+
+    def find(self, x):
+        if x != self.vertices[x][0]:
+            self.vertices[x][0] = self.find(self.vertices[x][0])
+        return self.vertices[x][0]
+
+    def union(self, x, y):
+        rootx = self.find(x)
+        rooty = self.find(y)
+        if self.vertices[rootx][1] > self.vertices[rooty][1]:
+            self.vertices[rooty][0] = rootx
+        else:
+            self.vertices[rootx][0] = rooty
+            if self.vertices[rootx][1] == self.vertices[rooty][1]:
+                self.vertices[rooty][1] += 1
 
 
 def solve(G):
@@ -14,55 +35,40 @@ def solve(G):
            T: networkx.Graph
        """
 
-    class DisjointSet:
-        def __init__(self, vertices):
-            self.vertices = [[v, 0] for v in range(vertices)]
-
-        def contains(self, vertex):
-            return vertex in [x[0] for x in self.vertices]
-
-        def find(self, x):
-            if x != self.vertices[x][0]:
-                self.vertices[x][0] = self.find(self.vertices[x][0])
-            return self.vertices[x][0]
-
-        def union(self, x, y):
-            rootx = self.find(x)
-            rooty = self.find(y)
-            if self.vertices[rootx][1] > self.vertices[rooty][1]:
-                self.vertices[rooty][0] = rootx
-            else:
-                self.vertices[rootx][0] = rooty
-                if self.vertices[rootx][1] == self.vertices[rooty][1]:
-                    self.vertices[rooty][1] += 1
 
     degrees = list((G.degree(G.nodes())))
     degrees.sort(key=lambda i:i[1], reverse=True)
 
-    max_degree = degrees[0][0]
+    curr_node = degrees[0][0]
 
     check_tree = DisjointSet(len(G.nodes))
 
+    result = nx.Graph()
+    result.add_node(curr_node)
 
-    # avg_edgeweights = 0
-    # node = 0
-    # for i in range(len(G.nodes)):
-    #     total = 0
-    #     for j in range(len(list(G.edges([i])))):
-    #         total += G.edges[list(G.edges([i]))[j]]['weight']
-    #     if total > max_edgeweights:
-    #         max_edgeweights = total
+    while not nx.is_dominating_set(G):
+        edge_set = list(G.edges([curr_node]))
+        smallest_pairwise_dist = float('inf')
+        best_edge = None
+        for edge in edge_set:
+            if check_tree.find(curr_node) != check_tree.find(edge[1]) and curr_node not in list(result.nodes):
+                result.add_node(edge[1])
+                result.add_edge(edge[0],edge[1])
+                if average_pairwise_distance_fast(result) < smallest_pairwise_dist:
+                    best_edge = edge
+                result.remove_node(edge[1])
+        result.add_node(best_edge[1])
+        result.add_edge(best_edge[0], best_edge[1])
+        curr_node = best_edge[1]
 
-    # order.sort(key=lambda x: x[1],reverse=True)
-    # seen = set()
-    # tree = nx.Graph()
-    # k = 0
-    # while len(seen) != len(G.nodes) and nx.is_connected(tree):
-    #     node = order[k][0]
-    #     for vertex in list(G.adj[node]):
-    #         seen.add(vertex)
+    return result
 
+def solve(G):
+    degrees = list((G.degree(G.nodes())))
+    degrees.sort(key=lambda i:i[1], reverse=True)
 
+    curr_node = degrees[0][0]
+    result = nx.dominating_set(G,[curr_node])
 
 
 
