f6bcab589085b60380e3dd7bafa830a41a1c426d
Connie Huang <conhuang@berkeley.edu>
2020-05-02 22:48:15 -0700

    Updated sort

--- solver.py
+++ solver.py
@@ -17,13 +17,6 @@ def solve(G):
     class DisjointSet:
         def __init__(self, vertices):
             self.vertices = [[v, 0] for v in range(vertices)]
-            #         self.vertexset = [v for v in vertices]
-
-        #     def add(self, vertex):
-        #         if self.contains(vertex):
-        #             return False
-        #         self.vertices.append([vertex, 0])
-        #         return True
 
         def contains(self, vertex):
             return vertex in [x[0] for x in self.vertices]
@@ -43,21 +36,31 @@ def solve(G):
                 if self.vertices[rootx][1] == self.vertices[rooty][1]:
                     self.vertices[rooty][1] += 1
 
+    degrees = list((G.degree(G.nodes())))
+    degrees.sort(key=lambda i:i[1], reverse=True)
 
-    order = []
-    for i in range(len(G.nodes)):
-        total = 0
-        for j in range(len(list(G.edges([i])))):
-            total += G.edges[list(G.edges([i]))[j]]['weight']
-        order.append((i, total))
-    order.sort(key=lambda x: x[1],reverse=True)
-    seen = set()
-    tree = nx.Graph()
-    k = 0
-    while len(seen) != len(G.nodes) and nx.is_connected(tree):
-        node = order[k][0]
-        for vertex in list(G.adj[node]):
-            seen.add(vertex)
+    max_degree = degrees[0][0]
+
+    check_tree = DisjointSet(len(G.nodes))
+
+
+    # avg_edgeweights = 0
+    # node = 0
+    # for i in range(len(G.nodes)):
+    #     total = 0
+    #     for j in range(len(list(G.edges([i])))):
+    #         total += G.edges[list(G.edges([i]))[j]]['weight']
+    #     if total > max_edgeweights:
+    #         max_edgeweights = total
+
+    # order.sort(key=lambda x: x[1],reverse=True)
+    # seen = set()
+    # tree = nx.Graph()
+    # k = 0
+    # while len(seen) != len(G.nodes) and nx.is_connected(tree):
+    #     node = order[k][0]
+    #     for vertex in list(G.adj[node]):
+    #         seen.add(vertex)
 
 
 
